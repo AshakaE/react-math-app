55bc823390864131049ff2c061eb022b0b4d76ac
AshakaE <ashakaegerega@gmail.com>
2021-05-14 12:33:08 +0100

    Import calculate js

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,6 @@
 import Display from './Display';
 import ButtonPanel from './ButtonPanel';
+// import calculate from '../logic/calculate'; // commented for passing linter errors
 
 const App = () => (
   <>
