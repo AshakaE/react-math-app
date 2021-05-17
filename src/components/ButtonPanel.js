c7533a124bb50e1b1433649d6e5a3f5d683f906b
AshakaE <ashakaegerega@gmail.com>
2021-05-17 15:47:39 +0100

    Add style for Button component

--- src/components/ButtonPanel.js
+++ src/components/ButtonPanel.js
@@ -8,34 +8,30 @@ const ButtonPanel = ({ clickHandler }) => (
       <Button name="AC" clickHandler={clickHandler} />
       <Button name="+/-" clickHandler={clickHandler} />
       <Button name="%" clickHandler={clickHandler} />
-      <Button name="÷" clickHandler={clickHandler} />
+      <Button color="orange" name="÷" clickHandler={clickHandler} />
     </div>
     <div className="flex w-[700px] h-[100px] justify-around">
-      <Button className="h-[100px]" name="7" clickHandler={clickHandler} />
-      <Button className="h-[100px]" name="8" clickHandler={clickHandler} />
-      <Button className="h-[100px]" name="9" clickHandler={clickHandler} />
-      <Button className="h-[100px]" name="*" clickHandler={clickHandler} />
+      <Button name="7" clickHandler={clickHandler} />
+      <Button name="8" clickHandler={clickHandler} />
+      <Button name="9" clickHandler={clickHandler} />
+      <Button color="orange" name="*" clickHandler={clickHandler} />
     </div>
     <div className="flex w-[700px] h-[100px] justify-around">
-      <Button className="h-[100px]" name="4" clickHandler={clickHandler} />
-      <Button className="h-[100px]" name="5" clickHandler={clickHandler} />
-      <Button className="h-[100px]" name="6" clickHandler={clickHandler} />
-      <Button className="h-[100px]" name="-" clickHandler={clickHandler} />
+      <Button name="4" clickHandler={clickHandler} />
+      <Button name="5" clickHandler={clickHandler} />
+      <Button name="6" clickHandler={clickHandler} />
+      <Button color="orange" name="-" clickHandler={clickHandler} />
     </div>
     <div className="flex w-[700px] h-[100px] justify-around">
-      <Button className="h-[100px]" name="1" clickHandler={clickHandler} />
-      <Button className="h-[100px]" name="2" clickHandler={clickHandler} />
-      <Button className="h-[100px]" name="3" clickHandler={clickHandler} />
-      <Button className="h-[100px]" name="+" clickHandler={clickHandler} />
+      <Button name="1" clickHandler={clickHandler} />
+      <Button name="2" clickHandler={clickHandler} />
+      <Button name="3" clickHandler={clickHandler} />
+      <Button color="orange" name="+" clickHandler={clickHandler} />
     </div>
     <div className="flex w-[700px] h-[100px] justify-between">
-      <div className="flex w-2/4 justify-around">
-        <Button className="h-[100px]" name="0" clickHandler={clickHandler} />
-      </div>
-      <div className="flex w-2/4 justify-around">
-        <Button className="h-[100px]" name="." clickHandler={clickHandler} />
-        <Button className="h-[100px]" name="=" clickHandler={clickHandler} />
-      </div>
+      <Button wide="wide" name="0" clickHandler={clickHandler} />
+      <Button name="." clickHandler={clickHandler} />
+      <Button color="orange" name="=" clickHandler={clickHandler} />
     </div>
   </>
 );
