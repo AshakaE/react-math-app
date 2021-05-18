5fcb9ce2128d9bf94b3f5454bc6741d6af1c0371
AshakaE <ashakaegerega@gmail.com>
2021-05-18 10:47:49 +0100

    Add api for getting quotes

--- src/components/Quote.js
+++ src/components/Quote.js
@@ -1,16 +1,33 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Navbar from './Navbar';
+import getQuote from './getQuote';
 
-const Quote = () => (
-  <>
-    <div>
-      <Navbar />
-    </div>
-    <div className="m-auto px-56 mt-60 text-center">
-      <p className="text-3xl font-light italic">Be the chief but never the lord.</p>
-      <h4 className="text-4xl pt-3">Lao Tzu</h4>
-    </div>
-  </>
-);
+const Quote = () => {
+  const [quote, setQuote] = useState('');
+
+  useEffect(() => {
+    getQuote().then((items) => {
+      const random = Math.floor(Math.random() * items.length);
+      const qday = items[random];
+      if (quote === '') {
+        setQuote(qday);
+      }
+    });
+  }, []);
+
+  return (
+    <>
+      <div>
+        <Navbar />
+      </div>
+      <div className="m-auto px-56 mt-60 text-center">
+        <div>
+          <p className="text-4xl font-extralight italic">{quote.text}</p>
+          <h4 className="text-3xl pt-3">{quote.author}</h4>
+        </div>
+      </div>
+    </>
+  );
+};
 
 export default Quote;
