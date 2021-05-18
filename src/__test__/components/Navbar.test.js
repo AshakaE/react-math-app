1c962c87d75cfbb9df64d7bb84d0e774fe042d9b
AshakaE <ashakaegerega@gmail.com>
2021-05-18 16:04:30 +0100

    Add tests for Navbar js

---
+++ src/__test__/components/Navbar.test.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import { BrowserRouter as Router } from 'react-router-dom';
+import Nav from '../../components/Navbar';
+
+describe('Nav', () => {
+  it('renders correctly', () => {
+    const tree = renderer
+      .create(
+        <Router>
+          <Nav />
+        </Router>,
+      )
+      .toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+});
