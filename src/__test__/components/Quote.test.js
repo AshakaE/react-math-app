32362dbaf30b847ebec7cd45892d81e56cb98e9d
AshakaE <ashakaegerega@gmail.com>
2021-05-18 16:51:00 +0100

    Add readme file

--- src/__test__/components/Quote.test.js
+++ src/__test__/components/Quote.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 import Quote from '../../components/Quote';
 
 describe('Quote', () => {
--- src/components/getQuote.js
+++ src/components/getQuote.js
@@ -1,4 +1,3 @@
-/* eslint-disable no-console */
 function getQuote() {
   return fetch('https://type.fit/api/quotes')
     .then((response) => response.json());
